2bf76c0fc97860b0b9962bf8e4c149b37ccf7b3d
Nikita Starshinov <nstarshinov@yandex.ru>
2020-07-10 23:43:18 +0000

    fix crash when bot is banned by user

--- bot/src/updater.ts
+++ bot/src/updater.ts
@@ -7,8 +7,12 @@ export const updateAll = async (t: Telegram) => {
   const res = await DB.feedItems.selectUpdates()
   console.log('Updater: sending ' + res.rowCount + ' updates')
   res.rows.forEach(async row => {
-    const str = '' + row.item_title + ' — '  + row.feed_title + '\n\n' + row.item_url
-    await DB.subscriptions.updateLastSent(row.chat_id, row.feed_url, row.item_date)
-    t.sendMessage(row.chat_id, str, { parse_mode: 'HTML' })
+    try {
+      const str = '' + row.item_title + ' — '  + row.feed_title + '\n\n' + row.item_url
+      await DB.subscriptions.updateLastSent(row.chat_id, row.feed_url, row.item_date)
+      await t.sendMessage(row.chat_id, str, { parse_mode: 'HTML' })
+    } catch (e) {
+      console.log(`updater: update failed: ${e}`)
+    }
   })
 }
