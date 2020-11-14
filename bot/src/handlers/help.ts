929bbf9023936eeec3801e54ddd1eeaa9cc56b70
Nikita Starshinov <nstarshinov@yandex.ru>
2020-11-14 07:14:15 +0000

    Update help

--- bot/src/handlers/help.ts
+++ bot/src/handlers/help.ts
@@ -1,13 +1,21 @@
 import Ctx from '../shared/ctx'
 
-const help = `Send me links and I will follow them. No need to use any commands in private chat with bot but you can use /subscribe or /s in groups.
+const help = `
+<b>Subscribing</b>
+Send links to subscribe. No need to use any commands in private chat, but you can use /subscribe or /s in groups. G Reader can import OPML files from other RSS readers. 
 
-To unsubscribe, <b>reply</b> /unsubscribe or /u to any message in the feed you want to unsubscribe from, or provide a feed link direcly.
+<b>Unsubscribing</b>
+To unsubscribe, <b>reply</b> /unsubscribe or /u to any message in the feed you want to unsubscribe from. Alternatively, provide a feed link direcly, e.g. /u example.com/feed.xml.
 
+<b>Other</b>
 /list shows all your subscriptions
 /export exports an OPML file that can be imported in other readers
-/help shows this message`
+
+<b>Getting help</b>
+G Reader is still in early days of development, and I'd love to keep improving it. Feel free to reach out <a href="https://t.me/nikstar">directly</a>, or <a href="https://github.com/nikstar/greader">create an issue</a>.
+`
+
 
 export const handleHelp = async (ctx: Ctx) => { 
-  await ctx.reply(help, { parse_mode: 'HTML' })
+  await ctx.reply(help, { parse_mode: 'HTML', disable_web_page_preview: true })
 }
