89d3f2855877bbc72a6017dde4d6bf7a9f98d386
Nikita Starshinov <nstarshinov@yandex.ru>
2021-07-16 15:50:57 +0000

    Remove unused session

--- bot/src/shared/ctx.ts
+++ bot/src/shared/ctx.ts
@@ -1,13 +1,4 @@
 import { ContextMessageUpdate } from 'telegraf'
 
-export class Session {
-  counter: number
-  
-  constructor() {
-    this.counter = 0
-  }
-}
-
 export default interface Ctx extends ContextMessageUpdate {
-  session: Session
 }
